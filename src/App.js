44320a87f54ffea9526fd4cc46b473623302d344
harshithareddy123456 <vsharshi233@gmail.com>
2024-06-20 12:23:41 +0530

    grid lights

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [status, setStatus] = useState([]);
+  const gridarray = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1],
+  ];
+  const handleclick = (id) => {
+    setStatus([...status, id]);
+  };
+  const handleplay = () => {
+    const interval = setInterval(() => {
+      setStatus((status) => {
+        let statusdup = [...new Set(status)];
+        statusdup.pop();
+        if (statusdup.length === 0) {
+          clearInterval(interval);
+          setStatus([]);
+        }
+        return statusdup;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  };
+  console.log(status);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="grid-container">
+        {gridarray.flat().map((item, index) => (
+          <div
+            className={status.includes(index) ? "grid grid-active" : "grid"}
+            key={index}
+            onClick={() => handleclick(index)}
+          ></div>
+        ))}
+      </div>
+      <button onClick={handleplay} className="play-button">
+        play
+      </button>
     </div>
   );
 }
